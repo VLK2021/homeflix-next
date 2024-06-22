78b1cb95dd9a94dc2c8a6f0e65605880a3b169aa
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-06-22 14:25:30 +0300

    react modal

--- src/app/components/SingleMovie/SingleMovie.js
+++ src/app/components/SingleMovie/SingleMovie.js
@@ -2,6 +2,9 @@
 import {useRouter} from "next/navigation";
 
 import styles from './SingleMovie.module.css';
+import {useState} from "react";
+import clsx from "classnames";
+import Modal from 'react-modal';
 
 
 const SingleMovie = ({movie}) => {
@@ -10,21 +13,65 @@ const SingleMovie = ({movie}) => {
         statistics: {viewCount, likeCount}
     } = movie;
     const router = useRouter();
+    const [toggleLike, setToggleLike] = useState(false);
+    const [toggleDisLike, setToggleDisLike] = useState(false);
 
 
     return (
-        <main className={styles.wrap}>
-            <button onClick={() => router.back()}>back</button>
-            {title}
+        <main>
+            <Modal
+                isOpen={true}
+                contentLabel="Watch the video"
+                onRequestClose={() => router.back()}
+                className={styles.modal}
+                overlayClassName={styles.overlay}
+            >
+                <button onClick={() => router.back()}>back</button>
 
-            <iframe
-                id="ytplayer"
-                type="text/html"
-                width="100%"
-                height="360"
-                src={`https://www.youtube.com/embed/${id}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
-                frameBorder="0"
-            > </iframe>
+                <iframe
+                    id="ytplayer"
+                    className={styles.videoPlayer}
+                    type="text/html"
+                    width="100%"
+                    height="360"
+                    src={`https://www.youtube.com/embed/${id}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
+                    frameBorder="0"
+                > </iframe>
+
+                {/*<div className={styles.likeDislikeBtnWrapper}>*/}
+                {/*    <div className={styles.likeBtnWrapper}>*/}
+                {/*        <button onClick={handleToggleLike}>*/}
+                {/*            <div className={styles.btnWrapper}>*/}
+                {/*                /!*<Like selected={toggleLike}/>*!/*/}
+                {/*            </div>*/}
+                {/*        </button>*/}
+                {/*    </div>*/}
+                {/*    <button onClick={handleToggleDislike}>*/}
+                {/*        <div className={styles.btnWrapper}>*/}
+                {/*            /!*<DisLike selected={toggleDisLike}/>*!/*/}
+                {/*        </div>*/}
+                {/*    </button>*/}
+                {/*</div>*/}
+                <div className={styles.modalBody}>
+                    <div className={styles.modalBodyContent}>
+                        <div className={styles.col1}>
+                            <p className={styles.publishTime}>{publishTime}</p>
+                            <p className={styles.title}>{title}</p>
+                            <p className={styles.description}>{description}</p>
+                        </div>
+                        <div className={styles.col2}>
+                            <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                                <span className={styles.textColor}>Cast: </span>
+                                <span className={styles.channelTitle}>{channelTitle}</span>
+                            </p>
+                            <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                                <span className={styles.textColor}>View Count: </span>
+                                <span className={styles.channelTitle}>{viewCount}</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </Modal>
         </main>
     );
 };
